61d5366360a4f54f27eccf715958991ca5272c6e
sam <samson.bassett@gmail.com>
2017-04-28 23:11:24 -0700

    update to 1.11.2

--- src/main/java/com/brendanmint/randomore/GUI/ConfigGUIFactory.java
+++ src/main/java/com/brendanmint/randomore/GUI/ConfigGUIFactory.java
@@ -23,11 +23,23 @@ public class ConfigGUIFactory implements IModGuiFactory
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	@SuppressWarnings("deprecation")
 	@Override
 	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+  @Override
+  public boolean hasConfigGui() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  @Override
+  public GuiScreen createConfigGui(GuiScreen parentScreen) {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
